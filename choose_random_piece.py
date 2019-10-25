44ab14deeb5493c4cb788c62576b8c283189f1e6
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-10-25 10:46:03 -0500

    now reads from csv when available

--- choose_random_piece.py
+++ choose_random_piece.py
@@ -1,19 +1,29 @@
-from get_music_list import get_music_list
+import csv
+from os.path import exists
 from random import randint
+from get_music_list import get_music_list
 
 
 def get_random_song(music_list):
     random_index = randint(0, len(music_list))
-    try:
-        piece = music_list[random_index]
-        return piece
-    except Exception as get_random_song_exception:
-        raise get_random_song_exception
+    piece = music_list[random_index]
+    return piece[0]
+
+
+def get_pieces():
+    if not exists('music.csv'):
+        get_music_list()
+    with open('music.csv', encoding='utf-16') as music_file:
+        pieces = []
+        csv_reader = csv.reader(music_file)
+        for row in csv_reader:
+            pieces.append(row)
+        return pieces
 
 
 if __name__ == "__main__":
     try:
-        music_list = get_music_list()
-        print(get_random_song(music_list))
+        print(get_random_song(get_pieces()))
     except Exception as get_music_exception:
+        print("Error while getting song:")
         print(get_music_exception)
--- get_music_list.py
+++ get_music_list.py
@@ -2,33 +2,25 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
+
 # () => list
 def get_music_list():
     soup = BeautifulSoup(get_content(), 'html.parser')
     music_rows = soup.find_all('tr')
     music = []
     for mr in music_rows:
-        try:
-            music.append(mr.select('td')[1].get_text(strip=True))
-        except Exception as parsed_format_exception:
-            raise parsed_format_exception
+        music.append(mr.select('td')[1].get_text(strip=True))
     write_music_csv(music)
     return music
 
 
 # () => request object
 def get_content():
-    try:
-        return requests.get('https://www.classicfm.com/classical-100/full-list/').content
-    except Exception as get_content_exception:
-        raise get_content_exception
+    return requests.get('https://www.classicfm.com/classical-100/full-list/').content
 
 
 # () => void (write csv file)
 def write_music_csv(music):
-    try:
-        with open('music.csv', encoding='utf-16', mode='w') as music_file:
-            music_writer = csv.writer(music_file)
-            [music_writer.writerow([piece]) for piece in music]
-    except Exception as write_music_exception:
-        raise write_music_exception
+    with open('music.csv', encoding='utf-16', mode='w') as music_file:
+        music_writer = csv.writer(music_file)
+        [music_writer.writerow([piece]) for (index, piece) in enumerate(music) if index > 0]
